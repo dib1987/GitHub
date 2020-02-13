d415aec1459d7be0ffb9d2f964f2d4028dbadad7
dib1987 <d.mondal87@gmail.com>
2020-02-13 15:23:17 +0530

    first one

---
+++ src/Demo1/day1.java
@@ -0,0 +1,12 @@
+package Demo1;
+
+import org.testng.annotations.Test;
+
+public class day1 extends Basic {
+
+	
+	@Test
+	public void validateErrorMessage() {
+		System.out.println("hello");
+	}
+}
